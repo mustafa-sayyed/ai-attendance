79fb466c72f58fe168b1704d41fd3d7e6372ec6f
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2025-02-14 16:17:34 +0530

    Add Pricing Page

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,6 @@ import FeatureBlock from "./FeatureBlock";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
-<<<<<<< HEAD
-
-export { Navbar, Hero, FeatureBlock, Footer, CTA};
-=======
 import Pricing from "./Pricing";
+
 export { Navbar, Hero, FeatureBlock, Footer, CTA, Pricing };
->>>>>>> a3a865764d10f33784166811c0371e3d61e66919
